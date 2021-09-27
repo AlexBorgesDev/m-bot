2f32ab1679e28862c23b55375d460c50be203f2e
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2021-09-27 20:24:16 -0300

    Created the play command

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -2,6 +2,9 @@ import { Message } from 'discord.js'
 
 import { prefix } from '../configs/configs.json'
 
+/* Controllers */
+import playController from '../controllers/play.controller'
+
 async function messageCreate(message: Message): Promise<void> {
   if (!message.content.startsWith(prefix) || message.author.bot) return
 
@@ -9,6 +12,9 @@ async function messageCreate(message: Message): Promise<void> {
   const command = args.shift()?.toLowerCase()
 
   if (!command) return
+
+  if (command === 'play') await playController(args, message)
+  else await message.channel.send('**Command does not exist**')
 }
 
 export default messageCreate
