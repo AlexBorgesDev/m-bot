3c31cdf9152cec6c9c5f5816a9f8260ee2b10139
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2021-09-27 18:58:27 -0300

    SongQueue type has been changed

--- src/queues/song.queue.ts
+++ src/queues/song.queue.ts
@@ -3,11 +3,13 @@ import { Message, StageChannel, VoiceChannel } from 'discord.js'
 
 import { YTSearchResult } from '../services/search.service'
 
+export type SongObj = {
+  user: { name: string; imageURL?: string | null }
+  data: YTSearchResult
+}
+
 export type SongQueue = {
-  songs: {
-    user: { name: string; imageURL?: string | null }
-    data: YTSearchResult
-  }[]
+  songs: SongObj
   volume: number
   player?: AudioPlayer | null
   playing: boolean
