5066f4f2b35643d39b645fe706e5461eb1affa58
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2021-09-27 15:14:49 -0300

    The song queue has been created

---
+++ src/queues/song.queue.ts
@@ -0,0 +1,19 @@
+import { AudioPlayer, AudioResource, VoiceConnection } from '@discordjs/voice'
+import { Message, StageChannel, VoiceChannel } from 'discord.js'
+
+import { YTSearchResult } from '../services/search.service'
+
+export type SongQueue = {
+  songs: YTSearchResult[]
+  volume: number
+  player?: AudioPlayer
+  playing: boolean
+  textChannel: Message['channel']
+  voiceChannel: VoiceChannel | StageChannel
+  audioResource?: AudioResource
+  voiceConnection?: VoiceConnection
+}
+
+const songQueue = new Map<string, SongQueue>()
+
+export default songQueue
