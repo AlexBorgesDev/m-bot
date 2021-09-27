2dc7db7048946757221b03e615c0fea1840daae2
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2021-09-27 14:50:44 -0300

    Project started

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import { Client, Intents } from 'discord.js'
+import { config } from 'dotenv'
+
+// Read environment variables from .env file
+config()
+
+const client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MEMBERS,
+    Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+    Intents.FLAGS.GUILD_INTEGRATIONS,
+    Intents.FLAGS.GUILD_VOICE_STATES,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    Intents.FLAGS.GUILD_MESSAGE_TYPING,
+    Intents.FLAGS.DIRECT_MESSAGES,
+    Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+    Intents.FLAGS.DIRECT_MESSAGE_TYPING,
+  ],
+})
+
+client.once('ready', () => console.log(`Logged in as ${client.user?.tag}`))
+
+client.login(process.env.BOT_TOKEN)
