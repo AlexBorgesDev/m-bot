3ec743687fd6921be93d046fb28946908b258317
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2021-09-27 20:46:16 -0300

    The volume command was created

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -4,6 +4,7 @@ import { prefix } from '../configs/configs.json'
 
 /* Controllers */
 import playController from '../controllers/play.controller'
+import volumeController from '../controllers/volume.controller'
 
 async function messageCreate(message: Message): Promise<void> {
   if (!message.content.startsWith(prefix) || message.author.bot) return
@@ -14,6 +15,7 @@ async function messageCreate(message: Message): Promise<void> {
   if (!command) return
 
   if (command === 'play') await playController(args, message)
+  else if (command === 'volume') await volumeController(args, message)
   else await message.channel.send('**Command does not exist**')
 }
 
