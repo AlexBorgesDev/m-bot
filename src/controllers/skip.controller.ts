e61f03bdbf65fde2c9987151cbe14cc0688b9036
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2021-09-28 10:50:38 -0300

    The skip controller has been updated

--- src/controllers/skip.controller.ts
+++ src/controllers/skip.controller.ts
@@ -1,4 +1,5 @@
 import { Message } from 'discord.js'
+
 import songQueue from '../queues/song.queue'
 
 async function skipController(args: string[], message: Message) {
@@ -8,9 +9,14 @@ async function skipController(args: string[], message: Message) {
 
   const serverSongQueue = songQueue.get(guild.id)
 
-  if (!serverSongQueue || !serverSongQueue.player)
+  if (
+    !serverSongQueue ||
+    !serverSongQueue.player ||
+    !serverSongQueue.audioStream
+  )
     return await channel.send('There are no songs to skip!')
 
+  serverSongQueue.audioStream.destroy()
   serverSongQueue.player.stop(true)
 }
 
